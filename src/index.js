1244933c0f92ab8ed2d16081ab622717cae65bde
Jonathan Zong <jzong@mit.edu>
2021-08-13 17:44:09 -0400

    gapminder

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 exports.__esModule = true;
 var vega = require("vega");
 var vl = require("vega-lite");
+var scatterplot = require("./scatterplot.json");
 var initVega = function (vgSpec) {
     var runtime = vega.parse(vgSpec);
     window.view = new vega.View(runtime)
@@ -11,21 +12,68 @@ var initVega = function (vgSpec) {
         .hover() // Enable hover event processing
         .run(); // Update and render the view
 };
-var vlSpec = {
-    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-    "description": "A simple bar chart with embedded data.",
-    "data": {
-        "values": [
-            { "a": "A", "b": 28 }, { "a": "B", "b": 55 }, { "a": "C", "b": 43 },
-            { "a": "D", "b": 91 }, { "a": "E", "b": 81 }, { "a": "F", "b": 53 },
-            { "a": "G", "b": 19 }, { "a": "H", "b": 87 }, { "a": "I", "b": 52 }
-        ]
-    },
-    "mark": "bar",
-    "encoding": {
-        "x": { "field": "a", "type": "nominal", "axis": { "labelAngle": 0 } },
-        "y": { "field": "b", "type": "quantitative" }
-    }
+var vlaSpec = scatterplot;
+var vlaToVl = function (vlaSpec) {
+    return { vlSpec: vlaSpec, vlaComp: vlaSpec.time };
 };
-var vgSpec = vl.compile(vlSpec).spec;
+var injectVlaInVega = function (vlaComp, vgSpec) {
+    var newVgSpec = Object.assign({}, vgSpec);
+    var dataset = newVgSpec.data[0];
+    console.log(dataset);
+    var newDataset = {
+        "name": dataset.name + "_1",
+        "source": dataset.name,
+        "transform": [
+            {
+                "type": "filter",
+                "expr": "(datum[\"" + vlaComp.field + "\"]) == fyear"
+            }
+        ]
+    };
+    var newSignal = {
+        "name": "fyear",
+        "init": "(extent(domain('time'))[0])",
+        "on": [
+            {
+                "events": { "type": "timer", "throttle": 1000 },
+                "update": "fyear < (extent(domain('time'))[1]) ? fyear + 5 : extent(domain('time'))[0]"
+            }
+        ]
+    };
+    var newScale = {
+        "name": "time",
+        "type": "ordinal",
+        "domain": { "data": dataset.name, "field": vlaComp.field }
+    };
+    newVgSpec.data.push(newDataset);
+    newVgSpec.signals = newVgSpec.signals || [];
+    newVgSpec.signals.push(newSignal);
+    newVgSpec.marks[0].from.data = newDataset.name;
+    newVgSpec.scales.push(newScale);
+    console.log(JSON.stringify(newVgSpec, null, 2));
+    return newVgSpec;
+};
+var vlaStuff = vlaToVl(vlaSpec);
+var vgSpec = vl.compile(vlaStuff.vlSpec).spec;
+var injectedVgSpec = injectVlaInVega(vlaStuff.vlaComp, vgSpec);
+initVega(injectedVgSpec);
+window.view.addSignalListener('fyear', function (_, value) {
+    document.getElementById('year').innerHTML = value;
+});
+// const vlSpec: vl.TopLevelSpec = {
+//   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+//   "description": "A simple bar chart with embedded data.",
+//   "data": {
+//     "values": [
+//       {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
+//       {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
+//       {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
+//     ]
+//   },
+//   "mark": "bar",
+//   "encoding": {
+//     "x": {"field": "a", "type": "nominal", "axis": {"labelAngle": 0}},
+//     "y": {"field": "b", "type": "quantitative"}
+//   }
+// };
 initVega(vgSpec);
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,111 @@
 import * as vega from 'vega';
 import * as vl from 'vega-lite';
+import * as scatterplot from './scatterplot.json';
 
 const initVega = (vgSpec: vega.Spec) => {
   const runtime = vega.parse(vgSpec);
   (window as any).view = new vega.View(runtime)
-        .logLevel(vega.Warn) // Set view logging level
-        .initialize(document.getElementById('view')) // Set parent DOM element
-        .renderer('svg') // Set render type (defaults to 'canvas')
-        .hover() // Enable hover event processing
-        .run(); // Update and render the view
+    .logLevel(vega.Warn) // Set view logging level
+    .initialize(document.getElementById('view')) // Set parent DOM element
+    .renderer('svg') // Set render type (defaults to 'canvas')
+    .hover() // Enable hover event processing
+    .run(); // Update and render the view
 }
 
-const vlSpec: vl.TopLevelSpec = {
-  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-  "description": "A simple bar chart with embedded data.",
-  "data": {
-    "values": [
-      {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
-      {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
-      {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
-    ]
+type VlAnimationComponent = {
+  "field": string,
+  "scale": {
+    "type": "linear",
+    "range": [number, number]
   },
-  "mark": "bar",
-  "encoding": {
-    "x": {"field": "a", "type": "nominal", "axis": {"labelAngle": 0}},
-    "y": {"field": "b", "type": "quantitative"}
-  }
+  "continuity": { "field": string }
 };
 
-const vgSpec = vl.compile(vlSpec).spec;
+type VlAnimationSpec = vl.TopLevelSpec & { "time": VlAnimationComponent };
+
+const vlaSpec: VlAnimationSpec = scatterplot;
+
+const vlaToVl = (vlaSpec: VlAnimationSpec): { vlSpec: vl.TopLevelSpec, vlaComp: VlAnimationComponent } => {
+  return { vlSpec: vlaSpec, vlaComp: vlaSpec.time };
+}
+
+const injectVlaInVega = (vlaComp: VlAnimationComponent, vgSpec: vega.Spec): vega.Spec => {
+  const newVgSpec = Object.assign({}, vgSpec);
+  const dataset = newVgSpec.data[0];
+  console.log(dataset);
+
+  const newDataset: vega.Data = {
+    "name": dataset.name + "_1",
+    "source": dataset.name,
+    "transform": [
+      {
+        "type": "filter",
+        "expr": `(datum["${vlaComp.field}"]) == fyear`
+      }
+    ]
+  };
+
+  const newSignal: vega.Signal = {
+    "name": "fyear",
+    "init": "(extent(domain('time'))[0])",
+    "on": [
+      {
+        "events": { "type": "timer", "throttle": 1000 },
+        "update": "fyear < (extent(domain('time'))[1]) ? fyear + 5 : extent(domain('time'))[0]"
+      }
+    ]
+  };
+
+  const newScale: vega.Scale =
+  {
+    "name": "time",
+    "type": "ordinal", // lol
+    "domain": { "data": dataset.name, "field": vlaComp.field }
+  };
+
+  newVgSpec.data.push(newDataset);
+  newVgSpec.signals = newVgSpec.signals || [];
+  newVgSpec.signals.push(newSignal);
+
+  newVgSpec.marks[0].from.data = newDataset.name;
+
+  newVgSpec.scales.push(newScale);
+
+  console.log(JSON.stringify(newVgSpec, null, 2));
+
+  return newVgSpec;
+}
+
+const vlaStuff = vlaToVl(vlaSpec);
+
+const vgSpec = vl.compile(vlaStuff.vlSpec).spec;
+
+const injectedVgSpec = injectVlaInVega(vlaStuff.vlaComp, vgSpec);
+
+initVega(injectedVgSpec);
+
+(window as any).view.addSignalListener('fyear', (_, value: string) => {
+  document.getElementById('year').innerHTML = value;
+})
+
+
+// const vlSpec: vl.TopLevelSpec = {
+//   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+//   "description": "A simple bar chart with embedded data.",
+//   "data": {
+//     "values": [
+//       {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
+//       {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
+//       {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
+//     ]
+//   },
+//   "mark": "bar",
+//   "encoding": {
+//     "x": {"field": "a", "type": "nominal", "axis": {"labelAngle": 0}},
+//     "y": {"field": "b", "type": "quantitative"}
+//   }
+// };
+
+
 
 initVega(vgSpec);
