d3a7ac34c916391265ca9f25739942e98111bd55
Jonathan Zong <jzong@mit.edu>
2021-08-13 16:08:34 -0400

    it works

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
 import * as vega from 'vega';
+import * as vl from 'vega-lite';
 
-let words: string[] = ['Hello', 'World'];
-
-for (const word of words) {
-  console.log(word);
+const initVega = (vgSpec: vega.Spec) => {
+  const runtime = vega.parse(vgSpec);
+  (window as any).view = new vega.View(runtime)
+        .logLevel(vega.Warn) // Set view logging level
+        .initialize(document.getElementById('view')) // Set parent DOM element
+        .renderer('svg') // Set render type (defaults to 'canvas')
+        .hover() // Enable hover event processing
+        .run(); // Update and render the view
 }
+
+const vlSpec: vl.TopLevelSpec = {
+  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+  "description": "A simple bar chart with embedded data.",
+  "data": {
+    "values": [
+      {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
+      {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
+      {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
+    ]
+  },
+  "mark": "bar",
+  "encoding": {
+    "x": {"field": "a", "type": "nominal", "axis": {"labelAngle": 0}},
+    "y": {"field": "b", "type": "quantitative"}
+  }
+};
+
+const vgSpec = vl.compile(vlSpec).spec;
+
+initVega(vgSpec);
