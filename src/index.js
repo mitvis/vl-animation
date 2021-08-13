db8280b24dab89a000c69bf112867f0e5398c4d8
Josh Pollock <jopo@mit.edu>
2021-08-13 15:41:45 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var words = ['Hello', 'World'];
+for (var _i = 0, words_1 = words; _i < words_1.length; _i++) {
+    var word = words_1[_i];
+    console.log(word);
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as vega from 'vega';
+
+let words: string[] = ['Hello', 'World'];
+
+for (const word of words) {
+  console.log(word);
+}
