2bb9bfe50ce7987a207eaf2d3cbb02fc37cdc128
Jonathan Zong <jzong@mit.edu>
2021-08-14 00:34:41 -0400

    gapminder tweening

--- src/index.ts
+++ src/index.ts
@@ -32,29 +32,99 @@ const vlaToVl = (vlaSpec: VlAnimationSpec): { vlSpec: vl.TopLevelSpec, vlaComp: 
 const injectVlaInVega = (vlaComp: VlAnimationComponent, vgSpec: vega.Spec): vega.Spec => {
   const newVgSpec = Object.assign({}, vgSpec);
   const dataset = newVgSpec.data[0];
-  console.log(dataset);
+  const scales = newVgSpec.scales;
 
-  const newDataset: vega.Data = {
-    "name": dataset.name + "_1",
-    "source": dataset.name,
-    "transform": [
-      {
-        "type": "filter",
-        "expr": `(datum["${vlaComp.field}"]) == fyear`
-      }
-    ]
-  };
+  const formulaTransforms: vega.FormulaTransform[] = scales.filter(s => (s.domain as vega.ScaleDataRef)?.field).map(s => {
+    const field = (s.domain as vega.ScaleDataRef).field;
+    return {
+      "type": "formula",
+      "as": `lerp_${field}`,
+      "expr": `lerp([datum.${field}, datum.next.${field}], fyear_tween)`
+    };
+  })
 
-  const newSignal: vega.Signal = {
-    "name": "fyear",
-    "init": "(extent(domain('time'))[0])",
-    "on": [
-      {
-        "events": { "type": "timer", "throttle": 1000 },
-        "update": "fyear < (extent(domain('time'))[1]) ? fyear + 5 : extent(domain('time'))[0]"
-      }
-    ]
-  };
+  const newDatasets: vega.Data[] = [
+    {
+      "name": dataset.name + "_1",
+      "source": dataset.name,
+      "transform": [
+        {
+          "type": "filter",
+          "expr": `(datum["${vlaComp.field}"]) == fyear`
+        }
+      ]
+    },
+    {
+      "name": dataset.name + "_2",
+      "source": dataset.name,
+      "transform": [
+        {
+          "type": "filter",
+          "expr": `(datum["${vlaComp.field}"]) == fyear2`
+        }
+      ]
+    },
+    {
+      "name": dataset.name + "_3",
+      "source": dataset.name + "_1",
+      "transform": [
+        {
+          "type": "lookup",
+          "from": dataset.name + "_2",
+          "key": vlaComp.continuity.field,
+          "fields": [vlaComp.continuity.field],
+          "as": ["next"]
+        },
+        ...formulaTransforms
+      ]
+    }
+  ]
+
+
+
+  const newSignals: vega.Signal[] = [
+    {
+      "name": "increment",
+      "value": 5
+    },
+    {
+      "name": "fyear",
+      "init": "(extent(domain('time'))[0])",
+      "on": [
+        {
+          "events": { "type": "timer", "throttle": 1000 },
+          "update": "fyear < (extent(domain('time'))[1]) ? fyear + increment : extent(domain('time'))[0]"
+        }
+      ]
+    },
+    {
+      "name": "fyear2",
+      "init": "fyear + increment",
+      "on": [
+        {
+          "events": {"signal": "fyear"},
+          "update": "min(extent(domain('time'))[1], fyear + increment)"
+        }
+      ]
+    },
+    {
+      "name": "fyear_tween",
+      "init": "0",
+      "on": [
+        {
+          "events": {
+            "type": "timer",
+            "throttle": 100
+          },
+          "update": "fyear_tween + 1/10"
+        },
+        {
+          "events": {"signal": "fyear"},
+          "update": "0"
+        }
+      ]
+    }
+  ];
 
   const newScale: vega.Scale =
   {
@@ -63,11 +133,17 @@ const injectVlaInVega = (vlaComp: VlAnimationComponent, vgSpec: vega.Spec): vega
     "domain": { "data": dataset.name, "field": vlaComp.field }
   };
 
-  newVgSpec.data.push(newDataset);
+  newVgSpec.data.push(...newDatasets);
   newVgSpec.signals = newVgSpec.signals || [];
-  newVgSpec.signals.push(newSignal);
+  newVgSpec.signals.push(...newSignals);
 
-  newVgSpec.marks[0].from.data = newDataset.name;
+  newVgSpec.marks[0].from.data = dataset.name + '_3';
+  Object.keys(newVgSpec.marks[0].encode.update).forEach(key => {
+    const maybeField = (newVgSpec.marks[0].encode.update[key] as any).field;
+    if (maybeField) {
+      (newVgSpec.marks[0].encode.update[key] as any).field = 'lerp_' + maybeField;
+    }
+  });
 
   newVgSpec.scales.push(newScale);
 
